985e1d1f34213f43b478b8b1a4a6156a33e6ef66
Candid Dauth <cdauth@cdauth.de>
2006-08-26 09:41:44 +0000

    Ant-Distribution erstellt.
    git-svn-id: svn://cdauth.de/sua/branches/v2_server@758 4c599e6c-1f03-0410-aeda-aa0b023f3d7a

---
+++ src/de/cdauth/sua/Galaxy.java
@@ -0,0 +1,15 @@
+package de.cdauth.sua;
+
+class Galaxy
+{
+	private int number;
+	private String[][] cache;
+
+	public Galaxy(int number, String database)
+	{
+	}
+
+	public void finalize()
+	{
+	}
+}
